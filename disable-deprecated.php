d60b650e402093ff6594e76e33c76f1cbef7541a
tamlyn <tamlyn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 15:28:41 +0000

    Initial commit.
    git-svn-id: https://plugins.svn.wordpress.org/disable-deprecated-warnings/trunk@562466 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ disable-deprecated.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	Plugin Name: Disable Deprecated Warnings for Plugins
+	Version: 1.0
+	Description: Prevents plugins from showing depracated errors in the WordPress admin when WP_DEBUG is on.
+	Author: tamlyn
+	Author URI: http://outlandishideas.co.uk
+*/
+
+function dd_load_plugin_first() {
+	// ensure path to this file is via main wp plugin path
+	$wp_path_to_this_file = preg_replace('/(.*)plugins\/(.*)$/', WP_PLUGIN_DIR."/$2", __FILE__);
+	$this_plugin = plugin_basename(trim($wp_path_to_this_file));
+	$active_plugins = get_option('active_plugins');
+	$this_plugin_key = array_search($this_plugin, $active_plugins);
+	if ($this_plugin_key) { // if it's 0 it's the first plugin already, no need to continue
+		array_splice($active_plugins, $this_plugin_key, 1);
+		array_unshift($active_plugins, $this_plugin);
+		update_option('active_plugins', $active_plugins);
+	}
+}
+add_action("activated_plugin", "dd_load_plugin_first");
+
+add_filter('deprecated_function_trigger_error', 'disable_all_deprecated_warnings');
+add_filter('deprecated_argument_trigger_error', 'disable_all_deprecated_warnings');
+add_filter('deprecated_file_trigger_error',     'disable_all_deprecated_warnings');
+function disable_all_deprecated_warnings($bool) {
+	return false;
+}
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Disable Deprecated Warnings for Plugins ===
+Contributors: tamlyn
+Tags: debug, depracated, errors, wp-debug
+Requires at least: 3.0
+Tested up to: 3.3
+Stable tag: trunk
+
+Prevents plugins from showing depracated errors in the WordPress admin.
+
+== Description ==
+
+As a theme developer, you want to have WP_DEBUG enabled but you're not interested in the deprecated warnings created by other plugins. Use this plugin to disable these messages while still displaying other errors, warnings and notices.
+
+[Plugin by Outlandish Ideas](http://outlandishideas.co.uk/)
+
+== Installation ==
+
+1. Install as usual.
+2. Activate plugin.
+
+The plugin has no admin screen, it just works silently in the background.
+
+== ChangeLog ==
+
+= Version 1.0.0 =
+
+* Initial release.
